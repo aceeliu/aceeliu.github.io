fefa6adb22d526b785f4c54ec37a2ef21277a40f
aceeliu <63484313+aceeliu@users.noreply.github.com>
2025-04-22 03:00:21 +0000

    Deploying to gh-pages from @ aceeliu/aceeliu.github.io@966f6de1558fbca9e80d1b34eea2425dad844d34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-a-post-with-image-galleries",
+        
+          title: "a post with image galleries",
+        
+        description: "this is what included image galleries could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/photography-local/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
